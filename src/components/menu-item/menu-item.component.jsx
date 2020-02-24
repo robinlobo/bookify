e496ab00bf7b1ab488601b1e3e6b606498a62fd5
robinlobo <robinson_3012@yahoo.co.in>
2020-02-24 23:07:32 +0530

    update title case

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -9,7 +9,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
         backgroundImage: `url(${imageUrl})`
     }} />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{ title.toUpperCase() }</h1>
       <span className="subTitle">SHOP NOW</span>
     </div>
   </div>
